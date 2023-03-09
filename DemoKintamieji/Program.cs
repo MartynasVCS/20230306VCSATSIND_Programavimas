1aa6b721fadba10db8876a5f50e492f439a39fe6
Martynas Satas <m.satas@gmail.com>
2023-03-09 11:59:06 +0200

    Add project files.

---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Žemėlapis!");
+
+            // Vienos eilutės komentaras
+
+            /* Komentaras
+             * per
+             * kelias
+             * eilutes
+             */
+
+            // Deklaracija ir inicializacija vienu veiksmu
+            int amzius = 7;
+
+            // Deklaracija ir inicializacija atskirais veiksmais
+            string tekstoKintamasis;
+            tekstoKintamasis = "Labas";
+
+            // Paprastiems tipams nerekomenduojama naudoti var
+            var tekstas = "Sveiki";
+            tekstas = "Labas";
+
+            // var naudojame kažkokieks sudėtingiems/ilgiems tipams
+            var encoding = System.Text.Encoding.UTF8;
+        }
+    }
+}
