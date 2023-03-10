c52839286e925ccca83f941d219092b2a8a3104d
Martynas Satas <m.satas@gmail.com>
2023-03-10 12:45:02 +0200

    Add example of switch statement

---
+++ DemoSwitch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DemoSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Norint ĮVESTI lietuviškas raides reikia pasikeisti koduotę
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Įveskite gyvūno pavadinimą: ");
+            string gyvunas = Console.ReadLine();
+
+            switch (gyvunas)
+            {
+                case "katė":
+                    Console.WriteLine("Įvedėte gyvūną 'katė'");
+                    break;
+                case "šuo":
+                    Console.WriteLine("Įvedėte gyvūną 'šuo'");
+                    break;
+                case "pelė":
+                    Console.WriteLine("Įvedėte gyvūną 'pelė'");
+                    break;
+                default:
+                    Console.WriteLine($"Nežinau gyvūno '{gyvunas}'");
+                    break;
+            }
+
+
+        }
+    }
+}
