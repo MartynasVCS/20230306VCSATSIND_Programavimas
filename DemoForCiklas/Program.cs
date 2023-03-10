01820c89c28d4f752e98191f4a1410e98599f8a9
Martynas Satas <m.satas@gmail.com>
2023-03-10 12:58:39 +0200

    Add example of for loop usage

---
+++ DemoForCiklas/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace DemoForCiklas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Standartinis for ciklas, vidinis iteratorius pavadinimu i, pradedame nuo 0, didiname po vieną
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            // Vidinis iteratorius nebūtinai turi būti integer
+            for (string i = "a"; i != "aaaaaaaa"; i += "a")
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            // Vidinio kintamojo kitimo veiksmas nebūtinai turi būti aprašytas for sąlygoje
+            // Amžinas ciklas
+            //for (int i = 0; i <= 5;)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine();
+
+            // Vidinio kintamojo keitimo veiksmas ciklo viduje
+            for (int i = 0; i <= 5;)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            // Vidinio kintamojo keitimo ne vienetu
+            for (int i = 0; i <= 50; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
