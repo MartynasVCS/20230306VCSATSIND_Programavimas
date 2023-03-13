ac5bcf3c7d27c6549f1106b87016dd4c11f405d1
Martynas Satas <m.satas@gmail.com>
2023-03-13 14:20:28 +0200

    Add demo code for working with methods

---
+++ DemoMetodai/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DemoMetodai
+{
+    internal class Calculator
+    {
+        internal static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Rekursinis metodas (kviečia pats save)
+        internal static void DivisionByTwoUntilZero(int number)
+        {
+            Console.WriteLine(number);
+            // Svarbu turėti išėjimo sąlyga, kad nebūtų amžinas ciklas
+            if (number != 0 )
+            {
+                int numberDivided = number / 2;
+                DivisionByTwoUntilZero(numberDivided);
+            }
+        }
+    }
+}
---
+++ DemoMetodai/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DemoMetodai
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Pasisveikinimas();
+            Console.WriteLine();
+
+            // Statiniai metodai
+            Console.WriteLine(Calculator.Sum(5, 5));
+            Console.WriteLine(Calculator.Sum(2, 5));
+            Console.WriteLine(Calculator.Sum(10, 5));
+            Console.WriteLine(Calculator.Sum(100000000, 5));
+
+            // Nestatiniai metodai
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Difference(10, 5));
+            Console.WriteLine();
+
+            Calculator.DivisionByTwoUntilZero(10);
+            Console.WriteLine();
+
+            Calculator.DivisionByTwoUntilZero(1000);
+            Console.WriteLine();
+        }
+
+        internal static void Pasisveikinimas()
+        {
+            Console.WriteLine("Hello World!!!!");
+        }
+    }
+}
