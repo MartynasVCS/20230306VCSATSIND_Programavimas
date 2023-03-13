36b9f3c6c0b94d3d9804e17c59094550f2c057a1
Martynas Satas <m.satas@gmail.com>
2023-03-13 13:13:10 +0200

    Add demo for working with lists

---
+++ DemoSarasai/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoSarasai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> zodziai = new List<string>();
+            zodziai.Add("Labas");
+
+            Console.WriteLine(zodziai[0]);
+            //Console.WriteLine(zodziai[1]); // Nėra reikšmės su indeksu 1
+        }
+    }
+}
