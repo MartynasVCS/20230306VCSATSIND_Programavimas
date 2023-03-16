6f0464235aca58aef240ee55c6191ae5b3530579
Martynas Satas <m.satas@gmail.com>
2023-03-16 08:53:58 +0200

    Add solution for tasks 17-39

---
+++ DemoKlases/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DemoKlases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zmogus zmogusPetras = new Zmogus("Petras", 45, 1.80);
+            Zmogus zmogusSaulius = new Zmogus("Saulius");
+            Zmogus zmogusAnonimas = new Zmogus();
+
+            Console.WriteLine(zmogusPetras.vardas);
+            Console.WriteLine(zmogusPetras.ugis);
+            Console.WriteLine(zmogusPetras.amzius);
+            zmogusPetras.Prisistatymas();
+            zmogusPetras.Prisistatymas();
+            
+            Console.WriteLine();
+
+            Console.WriteLine(zmogusSaulius.vardas);
+            Console.WriteLine(zmogusSaulius.ugis);
+            Console.WriteLine(zmogusSaulius.amzius);
+            zmogusSaulius.Prisistatymas();
+            zmogusSaulius.Atsisveikinimas(zmogusPetras);
+
+            Console.WriteLine();
+            zmogusAnonimas.Prisistatymas();
+        }
+    }
+}
---
+++ DemoKlases/Zmogus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DemoKlases
+{
+    internal class Zmogus
+    {
+        internal string vardas { get; }
+        internal int amzius { get; }
+        internal double ugis { get; }
+
+        public Zmogus(string vardas, int amzius, double ugis)
+        {
+            this.vardas = vardas;
+            this.amzius = amzius;
+            this.ugis = ugis;
+        }
+
+        public Zmogus(string vardas)
+        {
+            this.vardas = vardas;
+        }
+
+        public Zmogus()
+        {
+
+        }
+
+        internal void Prisistatymas()
+        {
+            if (vardas != null)
+            {
+                Console.WriteLine($"Sveiki, mano vardas yra {vardas}.");
+            }
+            else
+            {
+                Console.WriteLine("I was never given a name :(");
+            }
+        }
+
+        internal void Atsisveikinimas()
+        {
+            Console.WriteLine("Viso gero.");
+        }
+
+        internal void Atsisveikinimas(Zmogus kitasZmogus)
+        {
+            Console.WriteLine($"Ate {kitasZmogus.vardas}");
+        }
+    }
+}
